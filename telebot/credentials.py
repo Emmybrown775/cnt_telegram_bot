85391ab3be01901de9cd467eb19e776d1adf8e97
Emmybrown775 <elumezeemma@gmail.com>
2022-12-03 21:48:13 +0100

    month problem fixed

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,2 @@
-bot_token = "5558023539:AAFS282Gk9yEuxHZ6hj6S2VA307eSkMOyGI"
-bot_user_name = "cnt_open_heavens_bot"
-URL = "https://test775888.herokuapp.com/"
+import os
+bot_token = os.environ.get("BOT_TOKEN")
