8d24dc97acc9da97e464f4f7a790b6eb0c9d9439
Emmybrown775 <elumezeemma@gmail.com>
2023-04-07 12:34:15 +0100

    Photo testing 8

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, request
 import telegram
 from telebot.credentials import bot_token, URL
 from wp import WP
+from telegram.ext import Updater, MessageHandler, Filters
 
-global bot
-global TOKEN
+
+
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
@@ -13,52 +14,63 @@ app = Flask(__name__)
 
 
 
-
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+
 
     chat_id = update.effective_message.chat.id
     msg_id = update.effective_message.message_id
 
     # Telegram understands UTF-8, so encode text for unicode compatibility
-    if update.effective_message.text is not None:
 
-        text = update.effective_message.text.encode('utf-8').decode()
-        # for debugging purposes only
-        print("got text message :", text)
-        # the first time you chat with the bot AKA the welcoming message
-        if text == "/start":
-            # print the welcoming message
-            bot_welcome = """
-               Welcome to The CNT Open Heavens Bot, the bot is used to get daily open heavens from the CNT website.
-               """
-            # send the welcoming message
-            bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+    text = update.effective_message.text.encode('utf-8').decode()
 
-        elif text == "/make_post":
-            wp = WP()
-            result = wp.post_op()
-            bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
-        elif text == "/get_date":
-            wp = WP()
-            result = wp.get_date()
-            bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
-        elif text == "/get_last_post":
-            wp = WP()
-            result = wp.get_last_post()
-            bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
-        else:
-            wrong = "There is no such command"
+    # for debugging purposes only
+    print("got text message :", text)
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
+       Welcome to The CNT Open Heavens Bot, the bot is used to get daily open heavens from the CNT website.
+       """
+        # send the welcoming message
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-            bot.sendMessage(chat_id=chat_id, text=wrong, reply_to_message_id=msg_id)
+    elif text == "/make_post":
+        wp = WP()
+        result = wp.post_op()
+        bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
+    elif text == "/get_date":
+        wp = WP()
+        result = wp.get_date()
+        bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
+    elif text == "/get_last_post":
+        wp = WP()
+        result = wp.get_last_post()
+        bot.sendMessage(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
+    else:
+        wrong = "There is no such command"
 
-    elif update.effective_message.photo is not None:
-        photo_file = update.effective_message.photo[-1].get_file()
-        photo_file.download("user_photo.jpg")
-        update.message.reply_photo(photo_file)
+        bot.sendMessage(chat_id=chat_id, text=wrong, reply_to_message_id=msg_id)
+
     return 'ok'
+
+
+def handle_image(update, context):
+    # Get the image file ID
+    image_file_id = update.message.photo[-1].file_id
+
+    # Resend the image to the user
+    bot.send_photo(chat_id=update.effective_chat.id, photo=image_file_id)
+
+
+# Create an updater and add the image message handler
+updater = Updater(token='YOUR_BOT_TOKEN_HERE', use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.photo, handle_image))
 
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
